fe75e3b2df29fbfda425ca21363fb1b716151ee8
andire120 <164429441+andire120@users.noreply.github.com>
2026-06-09 18:11:04 +0900

    oi

---
+++ type1/js/script.js
@@ -0,0 +1,10 @@
+$(function() {
+    //메뉴
+    $("nav>ul>li").mouseenter(function() {
+        $(this).children(".sub").stop().slideDown();
+    })
+    $("nav>ul>li").mouseleave(function() {
+        $('.sub').stop().slideUp();
+    })
+
+})
